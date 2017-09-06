73420925a702b3ca7e656e4c7b9c7bacec96ff47
Patrick Connolly <patrick.c.connolly@gmail.com>
2017-09-06 15:58:52 -0400

    Fixed debug room. Added hardcoded message.

--- bot.py
+++ bot.py
@@ -3,9 +3,10 @@ import json
 import requests
 import urllib
 import os
+import sys
 import logging
 
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.INFO,
         stream=sys.stdout)
 
 # Suppress InsecureRequestWarning
@@ -14,11 +15,10 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 ###VARIABLES THAT YOU NEED TO SET MANUALLY IF NOT ON HEROKU#####
 try:
-        MESSAGE = os.environ['WELCOME-MESSAGE']
         TOKEN = os.environ['SLACK-TOKEN']
         UNFURL = os.environ['UNFURL-LINKS']
+        DEBUG_CHANNEL_ID = os.environ.get('DEBUG_CHANNEL_ID', False)
 except:
-        MESSAGE = 'Manually set the Message if youre not running through heroku or have not set vars in ENV'
         TOKEN = 'Manually set the API Token if youre not running through heroku or have not set vars in ENV'
         UNFURL = 'FALSE'
 ###############################################################
@@ -27,30 +27,41 @@ def is_team_join(msg):
     return msg['type'] == "team_join"
 
 def is_debug_channel_join(msg):
-    return msg['type'] == "channel_joined" and msg['channel']['name'] == 'greetingslack'
+    return msg['type'] == "member_joined_channel" and msg['channel'] == DEBUG_CHANNEL_ID and msg['channel_type'] == 'C'
+
+def welcome_message():
+    message = open('WELCOME_MESSAGE.txt', 'r').read()
+    return message
 
 def parse_join(message):
     m = json.loads(message)
     if is_team_join(m) or is_debug_channel_join(m):
-        x = requests.get("https://slack.com/api/im.open?token="+TOKEN+"&user="+m["user"]["id"])
-        print(x)
+        user_id = m["user"]["id"] if is_team_join(m) else m["user"]
+        logging.debug(m)
+        x = requests.get("https://slack.com/api/im.open?token="+TOKEN+"&user="+user_id)
         x = x.json()
-        print(x)
         x = x["channel"]["id"]
-        print(x)
+        data = {
+                'token': TOKEN,
+                'channel': x,
+                'text': welcome_message(),
+                'parse': 'full',
+                'as_user': 'false',
+                'username': 'Auto-Greeter',
+                }
+
+        logging.debug(data)
+
         if (UNFURL.lower() == "false"):
-          xx = requests.post("https://slack.com/api/chat.postMessage?token="+TOKEN+"&channel="+x+"&text="+urllib.quote(MESSAGE)+"&parse=full&as_user=true&unfurl_links=false")
-          print(xx)
-        else:
-          xx = requests.post("https://slack.com/api/chat.postMessage?token="+TOKEN+"&channel="+x+"&text="+urllib.quote(MESSAGE)+"&parse=full&as_user=true")
-          print(xx)
-        logging.debug('\033[91m' + "HELLO SENT" + m["user"]["id"] + '\033[0m')
+          data = data.update({'unfurl_link': 'false'})
+
+        xx = requests.post("https://slack.com/api/chat.postMessage", data=data)
+        logging.debug('\033[91m' + "HELLO SENT" + user_id + '\033[0m')
 
 #Connects to Slacks and initiates socket handshake
 def start_rtm():
     r = requests.get("https://slack.com/api/rtm.start?token="+TOKEN, verify=False)
     r = r.json()
-    logging.debug(r)
     r = r["url"]
     return r
 
